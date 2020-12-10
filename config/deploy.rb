91e0d2b5de3558c21bc5b5e20d01922fdb263187
Fred Duby <fcd1@columbia.edu>
2020-12-10 10:59:25 -0500

    Add capistrano for deploy, just dev for now

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+lock '3.10.1'
+
+set :department, 'ldpd'
+set :application, 'feed2js'
+
+set :repo_name, "#{fetch(:department)}-#{fetch(:application)}"
+set :repo_url,  "git@github.com:cul/#{fetch(:repo_name)}.git"
+
+set :remote_user, "#{fetch(:department)}serv"
+
+set :deploy_name, "#{fetch(:application)}_#{fetch(:stage)}"
+set :deploy_to, "/opt/www/#{fetch(:department)}/#{fetch(:deploy_name)}"
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('html/magpie/cache')
+set :linked_dirs, fetch(:linked_dirs, []).push('html/magpie/cache_utf8')
+
